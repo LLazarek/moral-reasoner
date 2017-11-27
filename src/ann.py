d16404e361aaf291829b35438d2e6fac3d5436a5
Lukas Lazarek <LukasALazarek@gmail.com>
2017-11-26 22:52:57 -0500

    Cost function working

--- src/ann.py
+++ src/ann.py
@@ -11,6 +11,8 @@ INPUT_UNITS = 2
 OUTPUT_UNITS = 1
 # Hidden layers: 1
 HIDDEN_UNITS = INPUT_UNITS
+UNITS_IN_LAYER = [2, 2, 1]
+LAMBDA = 0.0001
 class Net(namedtuple('Net', ['input', 'hidden', 'output'])):
     @staticmethod
     def empty(input=np.zeros(INPUT_UNITS)):
@@ -33,10 +35,7 @@ def sigmoid(x):
 # theta: n x 1
 # x: m x n
 def h(theta, x):
-    # return sigmoid(x.dot(theta))
-    print(theta)
-    print(x)
-    return x.dot(theta)
+    return sigmoid(x.dot(theta))
 
 # calc_unit_outputs: Theta Sample -> Net
 # Theta: List[np.matrix[2x3]]
@@ -55,4 +54,23 @@ def calc_unit_outputs(theta, x):
 
     return filled
 
+def cost(theta, X, y):
+    m = len(y)
+    cost = 0
+    for i in range(m):
+        for k in range(OUTPUT_UNITS):
+            h_theta_x = calc_unit_outputs(theta, X[i]).output[k]
+            cost += y[i][k]*math.log(h_theta_x) + \
+                    (1 - y[i][k])*math.log(1 - h_theta_x)
 
+    cost /= -m
+
+    regularizer = 0
+    for l in range(2):
+        for i in range(UNITS_IN_LAYER[l]):
+            for j in range(UNITS_IN_LAYER[l+1]):
+                regularizer += theta[l][j,i]**2
+
+    regularizer *= LAMBDA/(2*m)
+
+    return cost + regularizer
