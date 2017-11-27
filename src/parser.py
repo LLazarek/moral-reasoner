d58b4caffd0a8af04b241c93c6a85757074f7762
Lukas Lazarek <LukasALazarek@gmail.com>
2017-11-27 16:16:07 -0500

    Changed data loading to make arrays out of JSON

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,7 @@
 # Parser loading data
 
 import json
+import numpy as np
 
 def load(path):
     with open(path, 'r') as f:
@@ -15,8 +16,13 @@ def normalize(data):
             "intend":3
         },
         "foreseeability":{
-            "low":0,
-            "high":1
+            "n":0,
+            "low":1,
+            "high":2
+        },
+        "verdict":{
+            "not guilty":0,
+            "guilty":1
         }
     }
     yes_no = {"y": 1, "n": 0}
@@ -25,7 +31,28 @@ def normalize(data):
             for case in data]
 
 def load_training():
-    return normalize(load("../data/train.json"))
+    return to_matrix(normalize(load("../data/train.json")))
 
 def load_test():
-    return normalize(load("../data/test.json"))
+    return to_matrix(normalize(load("../data/test.json")))
+
+def to_matrix(json_data):
+    X = []
+    y = []
+    for case in json_data:
+        # Map dict into list of tuples: (factor, value)
+        items = case.items()
+        # filter out verdict and caseId
+        factors = filter(lambda factor:\
+                         factor[0] != "verdict" and factor[0] != "caseId",
+                         items)
+        # insert verdict into y
+        y.append([case["verdict"]])
+        # Sort list by factor so that all Xs are consistent
+        sorted_factors = sorted(factors, key=lambda item: item[0])
+        # Map list into list of value, insert into X
+        values = map(lambda item: item[1], sorted_factors)
+        print(values)
+        X.append(values)
+
+    return (np.array(X), np.array(y))
