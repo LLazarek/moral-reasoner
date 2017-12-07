dd2bce766776ecdb3ed62c149b2793be907b958b
Lukas Lazarek <LukasALazarek@gmail.com>
2017-12-07 16:59:06 -0500

    Switched ANN to logistic regression: 86% test

--- src/tensor_flow_ann.py
+++ src/tensor_flow_ann.py
@@ -10,16 +10,16 @@ LEARN_RATE = 0.12
 
 def forward_propogate(hidden_size, X, theta0, theta1):
     biases_hidden = tf.get_variable('biases_hidden', shape=[hidden_size], initializer=tf.constant_initializer(0.1), dtype=tf.float32)
-    biases_output = tf.get_variable('biases_output', shape=[2], initializer=tf.constant_initializer(0.1), dtype=tf.float32)
+    biases_output = tf.get_variable('biases_output', shape=[1], initializer=tf.constant_initializer(0.1), dtype=tf.float32)
     hidden_activations = tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(X, theta0), biases_hidden))
-    return tf.nn.bias_add(tf.matmul(hidden_activations, theta1), biases_output)
+    return tf.sigmoid(tf.nn.bias_add(tf.matmul(hidden_activations, theta1), biases_output))
     # hidden_activations = tf.nn.sigmoid(tf.matmul(X, theta0))
     # return tf.matmul(hidden_activations, theta1)
 
 def train(X_train, y_train, X_test, y_test):
     input_size = X_train.shape[1]
-    hidden_size = 15
-    y_size = 2
+    hidden_size = input_size
+    y_size = 1
 
     X = tf.placeholder("float", shape=[None, input_size])
     y = tf.placeholder("float", shape=[None, y_size])
@@ -29,13 +29,14 @@ def train(X_train, y_train, X_test, y_test):
     theta0 = tf.Variable(tf.random_normal((X_train.shape[1],
                                            hidden_size),
                                           stddev=0.1))
-    theta1 = tf.Variable(tf.random_normal((hidden_size, 2),
+    theta1 = tf.Variable(tf.random_normal((hidden_size, 1),
                                           stddev=0.1))
 
     predicted_y = forward_propogate(hidden_size, X, theta0, theta1)
-    predict = tf.argmax(predicted_y, axis=1) # Get index of largest value
+    # predict = tf.argmax(predicted_y, axis=1) # Get index of largest value
+    predict = tf.round(predicted_y)
 
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,
+    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,
                                                                   logits=predicted_y))
     updates = tf.train.GradientDescentOptimizer(LEARN_RATE).minimize(cost)
     sess = tf.Session()
@@ -52,15 +53,15 @@ def train(X_train, y_train, X_test, y_test):
 
         # print(sess.run(theta0))
 
-        train_predicted = np.matrix(sess.run(predict, feed_dict={X: X_train, y: y_train})).T
-        train_expected = np.matrix(np.argmax(y_train, axis=1)) # Get index of largest value
+        train_predicted = sess.run(predict, feed_dict={X: X_train, y: y_train})
+        train_expected = np.matrix(y_train) # Get index of largest value
         if NOTIFY_CHANGE and np.array_equal(last_train_predicted, train_predicted):
             print("Train prediction has not changed!")
         last_train_predicted = train_predicted
         train_accuracy = np.mean(np.equal(train_predicted, train_expected))
 
         test_predicted = sess.run(predict, feed_dict={X: X_test, y: y_test})
-        test_expected = np.matrix(np.argmax(y_test, axis=1)) # Get index of largest value
+        test_expected = np.matrix(y_test) # Get index of largest value
         if NOTIFY_CHANGE and np.array_equal(last_test_predicted, test_predicted):
             print("Test prediction has not changed!")
         last_test_predicted = test_predicted
@@ -91,23 +92,24 @@ def main():
 
 
 
-    from sklearn import linear_model
-    # from sklearn.ensemble import GradientBoostingClassifier
+    # from sklearn import linear_model
+    # # from sklearn.ensemble import GradientBoostingClassifier
 
-    # clf = GradientBoostingClassifier()
-    clf = linear_model.LogisticRegression()
-    clf.fit(X_train, y_train.T.flatten())
-    train_result = clf.score(X_train, y_train.T.flatten())
-    test_result =  clf.score(X_test, y_test.T.flatten())
-    print("train: {}".format(train_result))
-    print("test: {}".format(test_result))
-    exit(1)
+    # # clf = GradientBoostingClassifier()
+    # clf = linear_model.LogisticRegression()
+    # # clf = linear_model.LinearRegression()
+    # clf.fit(X_train, y_train.T.flatten())
+    # train_result = clf.score(X_train, y_train.T.flatten())
+    # test_result =  clf.score(X_test, y_test.T.flatten())
+    # print("train: {}".format(train_result))
+    # print("test: {}".format(test_result))
+    # exit(1)
 
 
-    y_train = tf.Session().run(tf.one_hot(y_train, 2))
-    y_train = np.matrix(list(map(lambda x: x[0].tolist(), y_train)))
-    y_test = tf.Session().run(tf.one_hot(y_test, 2))
-    y_test = np.matrix(list(map(lambda x: x[0].tolist(), y_test)))
+    # y_train = tf.Session().run(tf.one_hot(y_train, 2))
+    # y_train = np.matrix(list(map(lambda x: x[0].tolist(), y_train)))
+    # y_test = tf.Session().run(tf.one_hot(y_test, 2))
+    # y_test = np.matrix(list(map(lambda x: x[0].tolist(), y_test)))
 
     # print(X_test)
     # print(y_test)
